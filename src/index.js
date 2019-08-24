1e6b21f869e641dd810c57a3be6c9a7c7a912c0c
Rodrigo <rcoto@protonmail.com>
2019-08-24 00:20:36 -0700

    Feat: SASS included

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 import "sanitize.css";
+import "./styles/styles.scss";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
