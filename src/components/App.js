8afd6a8f67c0aa7fb6ebaf47b15c682061e17908
Rodrigo <rcoto@protonmail.com>
2019-08-24 18:59:43 -0700

    Feat: router switch completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navigation from "./Navigation";
+import Home from "./Home";
 
 export class App extends Component {
   render() {
     return (
       <Router>
         <Navigation />
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
       </Router>
     );
   }
