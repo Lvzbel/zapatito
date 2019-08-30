cc7162e339ea1e0c1f8b4a9524e0bfd2086b1c6d
Rodrigo <rcoto@protonmail.com>
2019-08-29 22:44:43 -0700

    Feat: Home now displays promo images correctly

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,16 +3,21 @@ import Hero from "./Hero";
 import PromoImg from "./PromoImg";
 import "../styles/components/HomeStyles.scss";
 import "../styles/components/ButtonStyles.scss";
+import "../styles/layout/HomeStyles.scss";
 
 export class Home extends Component {
   render() {
     return (
       <div className="Home">
         <Hero />
-        <div style={{ height: "600px", display: "flex", width: "100%" }}>
-          <PromoImg />
-          <PromoImg />
-        </div>
+        <section className="Home__promo-images">
+          <div className="Home__promo-image">
+            <PromoImg />
+          </div>
+          <div className="Home__promo-image">
+            <PromoImg />
+          </div>
+        </section>
       </div>
     );
   }
