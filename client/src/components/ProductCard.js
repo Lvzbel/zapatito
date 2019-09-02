a1a21a17d25b2a593215060ad21820acf7defae4
Rodrigo <rcoto@protonmail.com>
2019-09-02 16:27:11 -0700

    Refac: ProductCard was changed to a functional component and now accepts props

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,36 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import shoe from "img/product_test_img.jpg";
 import "../styles/components/ButtonStyles.scss";
 import "../styles/components/ProductCardStyles.scss";
 
-export class ProductCard extends Component {
-  render() {
-    return (
-      <div className="ProductCard">
-        <figure className="ProductCard__figure">
-          <div className="ProductCard__container">
-            <img className="ProductCard__img" src={shoe} alt="shoe" />
-            <div className="ProductCard__overlay">
-              <Link to="#" className="ProductCard__cta btn btn__primary">
-                Shop Now!
-              </Link>
-            </div>
+function ProductCard({ name, image, price, category }) {
+  return (
+    <div className="ProductCard">
+      <figure className="ProductCard__figure">
+        <div className="ProductCard__container">
+          <img className="ProductCard__img" src={image} alt="shoe" />
+          <div className="ProductCard__overlay">
+            <Link to="#" className="ProductCard__cta btn btn__primary">
+              Shop Now!
+            </Link>
           </div>
-          <figcaption className="ProductCard__caption">
-            <div className="ProductCard__details">
-              <Link to="#" className="ProductCard__link">
-                REBEL (ARMY GREEN)
-              </Link>
-              <p className="ProductCard__price">$115.00</p>
-            </div>
-            <div className="ProductCard__separator"></div>
-            <p className="ProductCard__category">PERFORMANCE LIFESTYLE</p>
-          </figcaption>
-        </figure>
-      </div>
-    );
-  }
+        </div>
+        <figcaption className="ProductCard__caption">
+          <div className="ProductCard__details">
+            <Link to="#" className="ProductCard__link">
+              {name}
+            </Link>
+            <p className="ProductCard__price">{`$${price}0`}</p>
+          </div>
+          <div className="ProductCard__separator"></div>
+          <p className="ProductCard__category">{category}</p>
+        </figcaption>
+      </figure>
+    </div>
+  );
 }
 
 export default ProductCard;
