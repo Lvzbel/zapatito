f3053dc298ffd3baf800bed1b50f2519d021bad3
Rodrigo <rcoto@protonmail.com>
2019-09-01 23:53:02 -0700

    Feat: backend now working with a small static db

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const db = require("./db/products.json");
+
+const saleFinder = db => {
+  const sortedProducts = db.sort((a, b) => {
+    if (parseFloat(a.price) < parseFloat(b.price)) {
+      return -1;
+    }
+    if (parseFloat(a.price) > parseFloat(b.price)) {
+      return 1;
+    }
+    return 0;
+  });
+  return sortedProducts.splice(0, 9);
+};
+
+// STATIC FILES
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// ROUTES
+app.get("/api", (req, res) => {
+  res.json(db);
+});
+
+app.get("/api/shoes", (req, res) => {
+  const shoes = db.products.filter(product => product.category === "shoes");
+  res.json(shoes);
+});
+
+app.get("/api/backpacks", (req, res) => {
+  const backpack = db.products.filter(
+    product => product.category === "backpack"
+  );
+  res.json(backpack);
+});
+
+app.get("/api/shirts", (req, res) => {
+  const shirts = db.products.filter(product => product.category === "shirts");
+  res.json(shirts);
+});
+
+app.get("/api/pants", (req, res) => {
+  const pants = db.products.filter(product => product.category === "pants");
+  res.json(pants);
+});
+
+app.get("/api/sale", (req, res) => {
+  const saleProducts = saleFinder(db.products);
+  res.json(saleProducts);
+});
+
+const saleProducts = saleFinder(db.products);
+// Server running
+app.listen(3000);
---
+++ client/build/precache-manifest.af6ea047d3904ded3e2a9c19993f17c8.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48dd036e2671536b7c3babdc41f8535",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4e51f88d72140a98a5a",
+    "url": "/static/css/2.0139936c.chunk.css"
+  },
+  {
+    "revision": "8a460f7245e779c9580d",
+    "url": "/static/css/main.e8f11319.chunk.css"
+  },
+  {
+    "revision": "a4e51f88d72140a98a5a",
+    "url": "/static/js/2.0afb68a8.chunk.js"
+  },
+  {
+    "revision": "8a460f7245e779c9580d",
+    "url": "/static/js/main.83fe8758.chunk.js"
+  },
+  {
+    "revision": "1b47c7bad38d036c8c95",
+    "url": "/static/js/runtime~main.d83df130.js"
+  },
+  {
+    "revision": "eb83a8b540d9e6d5766ed6cf75eb4d76",
+    "url": "/static/media/about-1.eb83a8b5.jpg"
+  },
+  {
+    "revision": "081e5907521894b12fb5c43d26ccbff6",
+    "url": "/static/media/about-2.081e5907.jpg"
+  },
+  {
+    "revision": "2c01c24fe51416d00a78f55c51aa36ea",
+    "url": "/static/media/about-3.2c01c24f.jpg"
+  },
+  {
+    "revision": "108a1a0fdb1161a96958118b400a18a4",
+    "url": "/static/media/about-team.108a1a0f.jpg"
+  },
+  {
+    "revision": "14c590d124662eb50efa4c00f027b79c",
+    "url": "/static/media/fa-brands-400.14c590d1.eot"
+  },
+  {
+    "revision": "3e1b2a654a784ceb385157140b4ccd71",
+    "url": "/static/media/fa-brands-400.3e1b2a65.woff2"
+  },
+  {
+    "revision": "5e8aa9ea0ebcd2218178f554cdd8e6b0",
+    "url": "/static/media/fa-brands-400.5e8aa9ea.ttf"
+  },
+  {
+    "revision": "bfa9c38bd6081dafe7278dedc4aad1d9",
+    "url": "/static/media/fa-brands-400.bfa9c38b.svg"
+  },
+  {
+    "revision": "df02c782834b113d605d8329368737b4",
+    "url": "/static/media/fa-brands-400.df02c782.woff"
+  },
+  {
+    "revision": "285a9d2a28886ad64c4e45cbd733cf7c",
+    "url": "/static/media/fa-regular-400.285a9d2a.ttf"
+  },
+  {
+    "revision": "5623624dd1b017c66c29d1ac69cdcfa3",
+    "url": "/static/media/fa-regular-400.5623624d.woff"
+  },
+  {
+    "revision": "95f13e0be408d117bd3d9b366084a3ef",
+    "url": "/static/media/fa-regular-400.95f13e0b.svg"
+  },
+  {
+    "revision": "aa66d0e0e38c75666e98db33abae955e",
+    "url": "/static/media/fa-regular-400.aa66d0e0.eot"
+  },
+  {
+    "revision": "ac21cac3f22cc9642f5af32e0c750797",
+    "url": "/static/media/fa-regular-400.ac21cac3.woff2"
+  },
+  {
+    "revision": "3ded831d708bf90b2da86756551b5c1c",
+    "url": "/static/media/fa-solid-900.3ded831d.woff"
+  },
+  {
+    "revision": "42e1fbd2cf655a0b44a2dfae9ca2f8c1",
+    "url": "/static/media/fa-solid-900.42e1fbd2.eot"
+  },
+  {
+    "revision": "6ed5e3bc9018d2a03e2f1fadbf8a0a4a",
+    "url": "/static/media/fa-solid-900.6ed5e3bc.svg"
+  },
+  {
+    "revision": "896e20e26ad07dc63c9526ed814117e9",
+    "url": "/static/media/fa-solid-900.896e20e2.ttf"
+  },
+  {
+    "revision": "d6d8d5da9214dc7d46b297672a602d55",
+    "url": "/static/media/fa-solid-900.d6d8d5da.woff2"
+  },
+  {
+    "revision": "8aa3eb2bdb296f6f09ae22f2b118e956",
+    "url": "/static/media/hero.8aa3eb2b.jpg"
+  },
+  {
+    "revision": "b72bc2a36c060072ae5bf6a9319a3ea4",
+    "url": "/static/media/product_test_img.b72bc2a3.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "9d1ec3117e41a64914396175aa5d2703",
+    "url": "/static/media/traveler-bagpack.9d1ec311.jpg"
+  }
+]);
