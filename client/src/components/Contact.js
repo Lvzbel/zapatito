0f715745dc614ef73cdc224546d66870db585b14
Rodrigo <rcoto@protonmail.com>
2019-09-15 01:09:24 -0700

    Feat: contact us page has now basic structure

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,9 +1,42 @@
 import React, { Component } from "react";
+import { Form, Field } from "react-final-form";
+import Input from "./Input";
+import "styles/components/ContactStyles.scss";
+import FormValidation from "../utils/formVerification";
 
-export class Contact extends Component {
-  render() {
-    return <div>This Contact Us</div>;
-  }
-}
+const onSubmit = e => {
+  console.log(e.customer_id);
+};
+
+const validateName = e => {
+  const nameInput = new FormValidation("NAME");
+  const result = nameInput.validate(e);
+  return result.errorMessage;
+};
+
+const nameInput = ({ input, meta }) => (
+  <Input {...input} type="text" errorMessage={meta.touched && meta.error} />
+);
+
+const Contact = () => (
+  <Form
+    onSubmit={onSubmit}
+    render={({ handleSubmit }) => (
+      <div className="Contact">
+        <div className="Contact__content">
+          <form onSubmit={handleSubmit}>
+            <h2>Contact Us</h2>
+            <Field
+              name="customer_id"
+              component={nameInput}
+              validate={validateName}
+            />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+    )}
+  />
+);
 
 export default Contact;
