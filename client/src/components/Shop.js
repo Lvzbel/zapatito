fbd619ae60dec3f59f19fd80227f835f456dc047
Rodrigo <rcoto@protonmail.com>
2019-09-19 22:52:55 -0700

    Feat: basic Shop layout

--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import "styles/components/ShopStyles.scss";
 
 export class Shop extends Component {
   render() {
-    return <div>Shop Page</div>;
+    return (
+      <div className="Shop">
+        <div className="Shop__banner"></div>
+        <div className="Shop__content container">
+          <aside className="Shop__filter">Filter</aside>
+          <div className="Shop__items">Shop</div>
+        </div>
+      </div>
+    );
   }
 }
 
