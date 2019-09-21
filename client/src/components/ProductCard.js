b4ab88e3cfa52c103d783bd37188ac8ec5fd874e
Rodrigo <rcoto@protonmail.com>
2019-09-21 00:28:05 -0700

    Feat: ProductCard now takes and id a prop and link currently works

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -3,14 +3,17 @@ import { Link } from "react-router-dom";
 import "../styles/components/ButtonStyles.scss";
 import "../styles/components/ProductCardStyles.scss";
 
-function ProductCard({ name, image, price, category }) {
+function ProductCard({ id, name, image, price, category }) {
   return (
     <div className="ProductCard">
       <figure className="ProductCard__figure">
         <div className="ProductCard__container">
           <img className="ProductCard__img" src={image} alt="shoe" />
           <div className="ProductCard__overlay">
-            <Link to="#" className="ProductCard__cta btn btn__primary">
+            <Link
+              to={`/shop/item/${id}`}
+              className="ProductCard__cta btn btn__primary"
+            >
               Shop Now!
             </Link>
           </div>
