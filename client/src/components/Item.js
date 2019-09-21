ad310810bb38b7502a1456c672fc3c2722b1bb30
Rodrigo <rcoto@protonmail.com>
2019-09-21 00:32:10 -0700

    Feat: Item now renders partial information

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -12,7 +12,36 @@ export class Item extends Component {
   render() {
     const item = this.props.productsItem[this.props.productsItem.length - 1];
     console.log(item);
-    return <div className="Item"></div>;
+    if (item) {
+      return (
+        <div className="Item">
+          <div className="container">
+            <div className="Item__content">
+              <div className="Item__card">
+                <img
+                  className="Item__image"
+                  src={`/images/${item.image}.jpg`}
+                  alt={item.name}
+                />
+              </div>
+              <div className="Item__description">
+                <div className="Item__header">
+                  <h5 className="Item__name">{item.name}</h5>
+                  <p className="Item__price">{`$${item.price}0`}</p>
+                </div>
+                <hr />
+                <p className="Item__category">{item.category}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div className="Item">
+        <h2>Loading</h2>
+      </div>
+    );
   }
 }
 
