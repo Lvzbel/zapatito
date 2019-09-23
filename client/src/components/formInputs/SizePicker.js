b3b23654ddabc34bdac7b90d630ec6c702f8d1d7
Rodrigo <rcoto@protonmail.com>
2019-09-22 23:06:20 -0700

    Feat: SizePicker styling complete needs props

--- client/src/components/formInputs/SizePicker.js
+++ client/src/components/formInputs/SizePicker.js
@@ -9,24 +9,41 @@ export class SizePicker extends Component {
           <label htmlFor="size" className="SizePicker__label">
             Select Size
           </label>
-          <button className="SizePicker__button">
-            <i className="fas fa-plus"></i>
-          </button>
         </div>
         <div className="SizePicker__options">
-          <input type="radio" name="size" id="xl" />
+          <input
+            className="SizePicker__radio"
+            type="radio"
+            name="size"
+            id="xl"
+          />
           <label htmlFor="xl" className="SizePicker__option">
             XL
           </label>
-          <input type="radio" name="size" id="md" />
+          <input
+            className="SizePicker__radio"
+            type="radio"
+            name="size"
+            id="md"
+          />
           <label htmlFor="md" className="SizePicker__option">
             MD
           </label>
-          <input type="radio" name="size" id="ms" />
+          <input
+            className="SizePicker__radio"
+            type="radio"
+            name="size"
+            id="ms"
+          />
           <label htmlFor="ms" className="SizePicker__option">
             SM
           </label>
-          <input type="radio" name="size" id="xs" />
+          <input
+            className="SizePicker__radio"
+            type="radio"
+            name="size"
+            id="xs"
+          />
           <label htmlFor="xs" className="SizePicker__option">
             XS
           </label>
