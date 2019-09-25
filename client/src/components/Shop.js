f5f0509cf9224c91582ddcbf4f5042dfd26e6294
Rodrigo <rcoto@protonmail.com>
2019-09-24 22:47:10 -0700

    Feat: Shop is now connected to Redux

--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchAll } from "actions";
 import "styles/components/ShopStyles.scss";
 
 export class Shop extends Component {
+  componentDidMount() {
+    this.props.fetchAll();
+  }
+
   render() {
+    console.log(this.props);
     return (
       <div className="Shop">
         <div className="Shop__banner"></div>
@@ -15,4 +22,14 @@ export class Shop extends Component {
   }
 }
 
-export default Shop;
+const mapStateToProps = state => {
+  return {
+    productsAll: state.products.productsAll,
+    productsFiltered: []
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchAll }
+)(Shop);
