30ca9ed978161e29b7c33efa978b186652ab6d04
Rodrigo <rcoto@protonmail.com>
2019-09-25 00:08:13 -0700

    Feat: Shop products are now showing

--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchAll } from "actions";
+import ProductCard from "components/ProductCard";
 import "styles/components/ShopStyles.scss";
 
 export class Shop extends Component {
   componentDidMount() {
     this.props.fetchAll();
+  }
+
+  renderItems() {
+    return this.props.productsAll.slice(1, 22).map(item => (
+      <div className="Shop__item">
+        <ProductCard
+          key={item.id}
+          id={item.id}
+          image={`/images/${item.image}.jpg`}
+          price={item.price}
+          category={item.category}
+        />
+      </div>
+    ));
   }
 
   render() {
@@ -15,7 +30,9 @@ export class Shop extends Component {
         <div className="Shop__banner"></div>
         <div className="Shop__content container">
           <aside className="Shop__filter">Filter</aside>
-          <div className="Shop__items">Shop</div>
+          <div className="Shop__items">
+            {this.props.productsAll && this.renderItems()}
+          </div>
         </div>
       </div>
     );
