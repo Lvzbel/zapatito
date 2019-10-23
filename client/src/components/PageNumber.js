45e9dae121b12fc37623d994469265e53a8dcf66
Rodrigo <rcoto@protonmail.com>
2019-10-22 21:59:05 -0700

    Feat: page buttons now redirect correctly

--- client/src/components/PageNumber.js
+++ client/src/components/PageNumber.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
+import UrlParams from "utils/urlParams";
+import history from "history.js";
 import "styles/components/PageNumber.scss";
 
 export class PageNumber extends Component {
+  setPage(pageNumber) {
+    const pageObject = { page: pageNumber };
+    const currentParams = UrlParams.setParam(
+      pageObject,
+      window.location.search
+    );
+    const stringURL = UrlParams.buildURL(currentParams);
+    history.push(`/shop?${stringURL}`);
+  }
+
   render() {
-    return <button className="PageNumber">{this.props.number}</button>;
+    return (
+      <button
+        onClick={() => this.setPage(this.props.number)}
+        className="PageNumber"
+      >
+        {this.props.number}
+      </button>
+    );
   }
 }
 
