024a8905125ec02e3bec65512c48c1ffb6114b7f
Rodrigo <rcoto@protonmail.com>
2019-10-24 20:29:45 -0700

    Feat: removed logic from PageNumber

--- client/src/components/PageNumber.js
+++ client/src/components/PageNumber.js
@@ -1,20 +1,7 @@
 import React, { Component } from "react";
-import UrlParams from "utils/urlParams";
-import history from "history.js";
 import "styles/components/PageNumber.scss";
 
 export class PageNumber extends Component {
-  setPage(pageNumber) {
-    const pageObject = { page: pageNumber };
-    const currentParams = UrlParams.setParam(
-      pageObject,
-      window.location.search
-    );
-    const stringURL = UrlParams.buildURL(currentParams);
-    history.push(`/shop?${stringURL}`);
-    history.go();
-  }
-
   render() {
     const selected = this.props.selected ? " selected" : "";
 
