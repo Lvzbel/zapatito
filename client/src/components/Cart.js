72a1e6dc9fb81966f297d875dc99c562dee18860
Rodrigo <rcoto@protonmail.com>
2019-10-31 22:25:18 -0700

    Feat: created new Cart page

---
+++ client/src/components/Cart.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "styles/components/CartStyles.scss";
+
+export class Cart extends Component {
+  render() {
+    return <div className="Cart">Welcome to cart</div>;
+  }
+}
+
+export default Cart;
