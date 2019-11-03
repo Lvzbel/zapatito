28ec7f7e66acbbfdd5da5b4cf0a68fa717cfa82c
Rodrigo <rcoto@protonmail.com>
2019-11-02 18:59:34 -0700

    Feat: CartItem now takes a size(optional)

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -4,10 +4,11 @@ import "styles/components/CartItemStyles.scss";
 // This Module requires all to work
 // image = "string"
 // name = "string"
+// size = "string"
 // category = "string"
 // remove = function
 
-function CartItem({ image, name, category, remove }) {
+function CartItem({ image, name, size, category, remove }) {
   return (
     <div className="CartItem">
       <div className="CartItem__img-container">
@@ -19,6 +20,7 @@ function CartItem({ image, name, category, remove }) {
       </div>
       <div className="CartItem__info">
         <p className="CartItem__name">{name}</p>
+        {size && <p className="CartItem__size">{`Size: ${size}`}</p>}
         <p className="CartItem__category">{category}</p>
         <button onClick={remove} className="CartItem__remove">
           Remove
