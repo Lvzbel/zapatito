262e72ad9c2f1f67f182f95315ef4af95326b4a5
Rodrigo <rcoto@protonmail.com>
2019-11-07 19:42:13 -0800

    Feat: added top section that stores the close button within

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -6,13 +6,15 @@ function Modal(props) {
   return ReactDOM.createPortal(
     <div onClick={props.onDismiss} className="Modal">
       <div onClick={e => e.stopPropagation()} className="Modal__container">
-        <button
-          aria-label="Close"
-          onClick={props.onDismiss}
-          className="Modal__close"
-        >
-          <i class="Modal__icon fas fa-window-close"></i>
-        </button>
+        <div className="Modal__top-bar">
+          <button
+            aria-label="Close"
+            onClick={props.onDismiss}
+            className="Modal__close"
+          >
+            <i class="Modal__icon fas fa-window-close"></i>
+          </button>
+        </div>
       </div>
     </div>,
     document.querySelector("#modal")
