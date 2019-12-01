b8c13da359c620cb47b34f0ec6721961351ab76f
Rodrigo <rcoto@protonmail.com>
2019-11-30 19:48:56 -0800

    Fix: GoogleAuth finally fix

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -16,8 +16,9 @@ export class GoogleAuth extends Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
-          this.state.isSignedIn &&
-            this.onAuthChange({ isSignedIn: this.auth.isSignedIn.get() });
+          this.onAuthChange(this.auth.isSignedIn.get());
+          // this.state.isSignedIn &&
+          //   this.onAuthChange({ isSignedIn: this.auth.isSignedIn.get() });
           this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
